1e0033fabe3c41b8e226f4baafc80cc5dd94aceb
李樟取 <lizhangqu@weidian.com>
2017-07-14 21:43:43 +0800

    支持从intellij idea configuration 启动

--- lesson1/src/main/java/io/github/lizhangu/lesson1/Application.java
+++ lesson1/src/main/java/io/github/lizhangu/lesson1/Application.java
@@ -2,6 +2,8 @@ package io.github.lizhangu.lesson1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 import java.util.Arrays;
@@ -10,10 +12,15 @@ import java.util.Arrays;
  * Created by lizhangqu on 2017/7/14.
  */
 @SpringBootApplication
-public class Main {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Main.class, args);
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
